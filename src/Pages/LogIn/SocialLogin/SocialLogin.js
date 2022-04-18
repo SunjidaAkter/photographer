45015acda1b19abb1fa133cced90bc91313023c5
SunjidaAkter <sanjidabintesayid7@gmail.com>
2022-04-18 13:56:36 +0600

    social login button fixed

--- src/Pages/LogIn/SocialLogin/SocialLogin.js
+++ src/Pages/LogIn/SocialLogin/SocialLogin.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Loading from '../../Shared/Loading/Loading';
 import googleIcon from "../../../icons/googleIcon.png";
 
 const SocialLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const navigate = useNavigate();
+    const location = useLocation();
 
+    let from = location.state?.from?.pathname || "/";
     let errorElement;
 
     if (loading) {
@@ -20,7 +22,7 @@ const SocialLogin = () => {
     }
 
     if (user) {
-        navigate('/home');
+        navigate(from, { replace: true });
     }
     return (
         <div>
