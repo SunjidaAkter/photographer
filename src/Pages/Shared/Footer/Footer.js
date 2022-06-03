18eeb1f5cfef3207ddb3d9ec389b779d7662476b
SunjidaAkter <sanjidabintesayid7@gmail.com>
2022-06-04 05:32:26 +0600

    footer edited

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -7,9 +7,10 @@ import instagramIcon from "../../../icons/instagramIcon.png";
 
 const Footer = () => {
     return (
-        <div className='pt-3 contaner bg mb-0'>
+
+        <div className='pt-3 contaner bg mb-0 bg-dark'>
             <h6 className='text-center mt-3 mb-0'>Copyright &copy; 2022 INSHOT.</h6>;
-            <h6 className='text-center mt-0 mb-0 pb-2'> All rights reserved.</h6>
+            <h6 className='text-center mt-2 mb-0 pb-2'> All rights reserved.</h6>
             <div className='d-flex justify-content-center align-items-center'>
                 <h6 className='my-3 me-3'>Follow me on </h6>
                 <a
